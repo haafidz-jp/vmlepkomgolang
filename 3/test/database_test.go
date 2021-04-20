ac2d35e574267d16aa55195c9379858636ff88c0
Haafidz Nurul Salim <haafidz123@gmail.com>
2021-04-20 14:28:02 +0700

    perubahan act 3

--- 3/test/database_test.go
+++ 3/test/database_test.go
@@ -3,7 +3,7 @@ package test
 import (
 	"database/sql"
 	"fmt"
-	"haafidz_52418987_pert3/model" //sesuaikan dengan nama folder (case sensitive)
+	"repo/3/model" // sesuaikan dengan nama folder (case sensitive)
 	"testing"
 )
 
@@ -17,30 +17,21 @@ func init() {
 	defaultDB = "mysql"
 }
 func TestDatabase(t *testing.T) {
-
 	t.Run("Testing untuk membuat database", func(t *testing.T) {
 		db, err := model.Connect(username, password, host, defaultDB)
-
 		defer db.Close()
-
 		if err != nil {
 			t.Fatal(err)
 		}
-
 		err = model.CreateDB(db, namaDB)
-
 		if err != nil {
 			t.Fatal(err)
 		}
-
 	})
-	t.Run("Testing untuk memeriksa koneksi database", func(t *testing.
-		T) {
 
+	t.Run("Testing untuk memeriksa koneksi database", func(t *testing.T) {
 		db, err := model.Connect(username, password, host, defaultDB)
-
 		defer db.Close()
-
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -64,32 +55,24 @@ func TestDatabase(t *testing.T) {
 }
 
 func initDatabase() (*sql.DB, error) {
-
 	dbInit, err := model.Connect(username, password, host, defaultDB)
-
 	if err != nil {
 		fmt.Println("Gagal melakukan koneksi")
 	}
-
 	if err = model.DropDB(dbInit, namaDB); err != nil {
 		fmt.Println("Gagal menghapus database")
 		return nil, err
 	}
-
 	if err = model.CreateDB(dbInit, namaDB); err != nil {
 		fmt.Println("Gagal membuat database")
 		return nil, err
 	}
-
 	dbInit.Close()
-
 	db, err := model.Connect(username, password, password, namaDB)
-
 	if err != nil {
 		fmt.Println("Gagal melakukan koneksi")
 		return nil, err
 	}
-
 	if err = model.CreateTable(db, model.TabelMahasiswa); err != nil {
 		fmt.Println("Gagal membuat table mahasiswa")
 		return nil, err
--- 3/test/mahasiswa_test.go
+++ 3/test/mahasiswa_test.go
@@ -1,12 +1,11 @@
 package test
 
 import (
-	"haafidz_52418987_pert3/model" //sesuaikan dengan nama folder (case sensitive)
+	"repo/3/model" //sesuaikan dengan nama folder (case sensitive)
 	"testing"
 )
 
 func TestMahasiswa(t *testing.T) {
-
 	var dataInsertMhs = []model.Mahasiswa{
 		model.Mahasiswa{
 			NPM:   "52418987",
@@ -14,9 +13,9 @@ func TestMahasiswa(t *testing.T) {
 			Kelas: "3IA23",
 		},
 		model.Mahasiswa{
-			NPM:   "12345678",
-			Nama:  "Boedy",
-			Kelas: "3KA20",
+			NPM:   "19283746",
+			Nama:  "Doremi Budi",
+			Kelas: "4KA20",
 		},
 		model.Mahasiswa{
 			NPM:   "44444444",
@@ -25,11 +24,9 @@ func TestMahasiswa(t *testing.T) {
 		},
 	}
 	db, err := initDatabase()
-
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	t.Run("Testing insert mahasiswa", func(t *testing.T) {
 		for _, dataInsert := range dataInsertMhs {
 			err := dataInsert.Insert(db)
@@ -38,32 +35,26 @@ func TestMahasiswa(t *testing.T) {
 			}
 		}
 	})
-
 	t.Run("Testing update mahasiswa", func(t *testing.T) {
 		var updateData = map[string]interface{}{
-			"nama": "Abdi Teh Ayeuna",
-		}
-
+			"nama": "Abdi Teh Ayeuna"}
 		data := dataInsertMhs[0]
 		if err := data.Update(db, updateData); err != nil {
 			t.Fatal(err)
 		}
 	})
-
 	t.Run("Testing Get mahasiswa", func(t *testing.T) {
 		_, err := model.GetMahasiswa(db, "44444444")
 		if err != nil {
 			t.Fatal(err)
 		}
 	})
-
 	t.Run("Testing Get mahasiswa", func(t *testing.T) {
 		_, err := model.GetAllMahasiswa(db)
 		if err != nil {
 			t.Fatal(err)
 		}
 	})
-
 	t.Run("Testing delete mahasiswa", func(t *testing.T) {
 		data := dataInsertMhs[0]
 		if err := data.Delete(db); err != nil {
